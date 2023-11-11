1f0ab075f3e29d4d0ece50ab88e83b8ca69be4ad
Zach Willis <zachmw27@gmail.com>
2023-11-11 10:13:13 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+    return function(description = "special") {
+        return `You are ${symbol}${description}${symbol}!`;
+    }
+}
